b43f6a1144cd745e49bc5572d20bc30169349fe8
Woody <wytun8904@gmail.com>
2026-02-19 13:48:18 +0630

    application detail

---
+++ app/company/manage-internships/[id]/page.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import CompanyApplications from "../../applications/page";
+
+export default function InternshipApplications() {
+    return (
+        <CompanyApplications/>
+    );
+}
